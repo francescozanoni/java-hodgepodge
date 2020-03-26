034144fe6a142530f8cc988f35045730e9d4624b
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-03-26 12:54:24 +0100

    minor improvements

--- src/main/java/it/francescozanoni/app/FXMLExampleController.java
+++ src/main/java/it/francescozanoni/app/FXMLExampleController.java
@@ -31,16 +31,26 @@
  */
 
 package it.francescozanoni.app;
- 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
- 
+import javafx.scene.control.PasswordField;
+
 public class FXMLExampleController {
-    @FXML private Text actiontarget;
-    
-    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-        actiontarget.setText("Sign in button pressed");
+
+    @FXML
+    private Text actionTarget;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        if (passwordField.getText().equals("")) {
+            actionTarget.setText("EMPTY PASSWORD");
+            return;
+        }
+        actionTarget.setText("Sign in button pressed");
     }
 
 }
