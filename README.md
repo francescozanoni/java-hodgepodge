9d7096087ac599e614450d2feeef60f41aee4af7
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-12-13 13:00:59 +0100

    concurrent HTTP download example: refactored and moved URLs to JSON file

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Thorough documentation sources are listed within code comments.
   * `async`: asynchronicity, concurrency and lambda expressions
   * `cli`: command line app with [picocli](https://picocli.info) package
   * `concurrency/db`: concurrency while writing to database
-  * `concurrency/http`: web pages concurrent download, with IBAN code extraction via [regular expression](https://www.vogella.com/tutorials/JavaRegularExpressions/article.html)
+  * `concurrency/http`: web pages concurrent download, with URLs [loaded from JSON](https://howtodoinjava.com/java/library/json-simple-read-write-json-examples) file and IBAN code extraction via [regular expression](https://www.vogella.com/tutorials/JavaRegularExpressions/article.html)
   * `gui`: several [JavaFX](https://openjfx.io) examples
   * `App.java`: dumb application used for dumb test
 * `src/test/java/it/francescozanoni/app/AppTest.java`: dumb test of dumb application
--- src/main/java/it/francescozanoni/app/concurrency/http/Main.java
+++ src/main/java/it/francescozanoni/app/concurrency/http/Main.java
@@ -1,109 +1,68 @@
 package it.francescozanoni.app.concurrency.http;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.FileReader;
+import java.io.IOException;
 
-// @see https://howtodoinjava.com/java/multi-threading/executorservice-invokeall
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * Extract IBAN codes from several web pages.
  */
 public class Main {
 
-    private static final String[] URLS = {
-            "https://www.citadele.lt/en/support/online-banking/examples/",
-            "https://it.iban.com/struttura",
-            "https://transferwise.com/gb/iban/example?all=true#structure_and_examples",
-            "https://bank.codes/iban/examples/",
-            "http://iban.co.uk/examples.html",
-            "https://www.danskebank.ie/en-ie/Personal/Day-to-day/Payments/helpfulInformation/Pages/What-is-IBAN-SWIFT-BIC.aspx",
-            "https://www.hl.co.uk/investment-services/currency-service/ways-to-convert-currency/bank-transfer-information/iban-example",
-            "https://ssl.ibanrechner.de/sample_accounts.html",
-            "https://countrywisecodes.com/morocco/verify-iban-structure/MA64011519000001205000534921"
-    };
-    private static final String IBAN_PATTERN = "[A-Z]{2} ?[0-9]{2} ?[A-Z0-9 ]{4,}";
-    private static final int IBAN_MAX_LENGTH = 34;
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        // @see https://stackoverflow.com/questions/2593154/get-a-resource-using-getresource
+        // Objects.requireNonNull(...) has been suggested by IntelliJ IDEA.
+        String urlJsonFilePath = Objects.requireNonNull(Main.class.getClassLoader().getResource("iban_urls.json")).getPath();
 
-        ExecutorService executor = Executors.newFixedThreadPool(Main.URLS.length);
+        // Load web pages URLs from JSON file.
+        String[] urls = getStringArrayFromJsonFile(urlJsonFilePath);
 
-        List<Callable<String>> taskList = new ArrayList<>();
+        String[] ibanCodes = run(urls);
 
-        // Create all web page download tasks.
-        for (String url : Main.URLS) {
-            taskList.add(Main.getTask(url));
-        }
+        System.out.println(Arrays.toString(ibanCodes));
 
-        // Run all tasks and wait for all to complete.
-        List<Future<String>> resultList = executor.invokeAll(taskList);
+    }
 
-        // Alternative: throw a java.util.concurrent.CancellationException if it takes more than 5 seconds.
-        // List<Future<String>> resultList = executor.invokeAll(taskList, 5, TimeUnit.SECONDS);
+    public static String[] run(String[] urls) {
 
-        executor.shutdown();
+        String[] urlContents = Downloader.run(urls);
 
-        // @see https://www.vogella.com/tutorials/JavaRegularExpressions/article.html
-        Pattern pattern = Pattern.compile(Main.IBAN_PATTERN);
+        return IbanExtractor.run(urlContents);
 
-        ArrayList<String> ibans = new ArrayList<>();
+    }
 
-        // Extract unique IBANs from all downloaded web pages.
-        for (Future<String> future : resultList) {
-            String result = future.get();
-            Matcher matcher = pattern.matcher(result);
-            while (matcher.find()) {
-                String iban = Main.getCleanIban(matcher.group());
-                if (!ibans.contains(iban)) {
-                    ibans.add(iban);
-                }
+    public static String[] getStringArrayFromJsonFile(String jsonFilePath) {
+
+        String[] strings;
+
+        // @see https://howtodoinjava.com/java/library/json-simple-read-write-json-examples
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader(jsonFilePath)) {
+
+            JSONArray stringsAsJsonArray = (JSONArray) jsonParser.parse(reader);
+
+            // stringsAsJsonArray.toArray() cannot be used, as it triggers this exception:
+            //   Exception in thread "main" java.lang.ClassCastException: class java.lang.Object cannot be cast to class java.lang.String
+
+            strings = new String[stringsAsJsonArray.size()];
+            for (int i = 0; i < stringsAsJsonArray.size(); i++) {
+                strings[i] = stringsAsJsonArray.get(i).toString();
             }
+
+        } catch (IOException | ParseException e) {
+            strings = null;
         }
 
-        ibans.sort(String::compareTo);
+        return strings;
 
-        System.out.println(ibans);
-
-    }
-
-    /**
-     * Create a task that downloads the content of the input URL via HTTP.
-     *
-     * @param url URL of the web page to download
-     * @return download task
-     */
-    public static Callable<String> getTask(String url) {
-
-        return () -> {
-            HttpClient client = HttpClient.newBuilder().build();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("downloaded " + url);
-            return response.body();
-        };
-
-    }
-
-    /**
-     * Clean an IBAN string to standard form.
-     *
-     * @param iban raw IBAN code e.g. "DK50 0040 0440 1162 43 "
-     * @return standardized IBAN code e.g. "DK5000400440116243"
-     * @todo return non-empty strings only if IBAN matches its country pattern
-     */
-    public static String getCleanIban(String iban) {
-        iban = iban.replaceAll("[^0-9A-Z]+", "");
-        if (iban.length() > Main.IBAN_MAX_LENGTH) {
-            iban = iban.substring(0, Main.IBAN_MAX_LENGTH);
-        }
-        return iban;
     }
 
 }
